2a9225abde9dd2f3a94db0014b52a96ef9777ab7
Danielle Nourok <dnourok1@gmail.com>
2016-03-29 11:08:20 -0400

    first slide with jQuery functioning

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,7 @@ jQuery(document).ready(function ($) {
     //initialise Stellar.js
     $(window).stellar();
     //Cache some variables
-    var links = $('.navigation').find('li');
+    var links = $('.nav-pills').find('li');
     slide = $('.slide');
     button = $('.button');
     mywindow = $(window);
@@ -31,20 +31,20 @@ jQuery(document).ready(function ($) {
         //If the user scrolls up change the navigation link that has the same data-slide attribute as the slide to active and
         //remove the active class from the previous navigation link
         if (direction === 'down') {
-            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
+            $('.nav-pills li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
         }
         // else If the user scrolls down change the navigation link that has the same data-slide attribute as the slide to active and
         //remove the active class from the next navigation link
         else {
-            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
+            $('.nav-pills li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
         }
     });
     //waypoints doesnt detect the first slide when user scrolls back up to the top so we add this little bit of code, that removes the class
     //from navigation link slide 2 and adds it to navigation link slide 1.
     mywindow.scroll(function () {
         if (mywindow.scrollTop() == 0) {
-            $('.navigation li[data-slide="1"]').addClass('active');
-            $('.navigation li[data-slide="2"]').removeClass('active');
+            $('.nav-pills li[data-slide="1"]').addClass('active');
+            $('.nav-pills li[data-slide="2"]').removeClass('active');
         }
     });
     //Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery
