bcc6ea578382e03655508606969fcbe854edd7fe
Danielle Nourok <dnourok1@gmail.com>
2016-04-13 08:56:24 -0400

    production 4 heroku

--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,8 @@ Bundler.require(*Rails.groups)
 
 module ParallaxDesign
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end
 
 
-config.cache_classes = true
-config.serve_static_assets = true
-config.assets.compile = true
-config.assets.digest = true
 
